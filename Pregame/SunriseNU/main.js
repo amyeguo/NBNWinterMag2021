40a0970b6db3883d3df52704d49e04e942bb1002
amyeguo <72362221+amyeguo@users.noreply.github.com>
2021-05-04 09:54:37 -0500

    Updated content

--- Pregame/SunriseNU/main.js
+++ Pregame/SunriseNU/main.js
@@ -1,37 +1,40 @@
-function animate({ timing, draw, duration }) {
-  let start = performance.now();
+function animate({timing, draw, duration}) {
 
-  requestAnimationFrame(function animate(time) {
-    // timeFraction goes from 0 to 1
-    let timeFraction = (time - start) / duration;
-    if (timeFraction > 1) timeFraction = 1;
-
-    // calculate the current animation state
-    let progress = timing(timeFraction);
-
-    draw(progress); // draw it
-
-    if (timeFraction < 1) {
-      requestAnimationFrame(animate);
-    }
-  });
-}
+    let start = performance.now();
+  
+    requestAnimationFrame(function animate(time) {
+      // timeFraction goes from 0 to 1
+      let timeFraction = (time - start) / duration;
+      if (timeFraction > 1) timeFraction = 1;
+  
+      // calculate the current animation state
+      let progress = timing(timeFraction)
+  
+      draw(progress); // draw it
+  
+      if (timeFraction < 1) {
+        requestAnimationFrame(animate);
+      }
+  
+    });
+  }
 
 function linear(timeFraction) {
-  return timeFraction;
+    return timeFraction;
 }
 
 function draw(progress) {
-  train.style.left = progress + "px";
+    train.style.left = progress + 'px';
 }
-protest.onclick = function () {
-  animate({
-    duration: 1000,
-    timing: function (timeFraction) {
-      return timeFraction;
-    },
-    draw: function (progress) {
-      protest.style.width = progress * 50 + "%";
-    },
-  });
-};
+
+protest.onclick = function() {
+    animate({
+      duration: 1000,
+      timing: function(timeFraction) {
+        return timeFraction;
+      },
+      draw: function(progress) {
+        protest.style.width = progress * 100 + '%';
+      }
+    });
+  };
--- main.js
+++ main.js
@@ -24,6 +24,99 @@ for (let i = 0; i < btnsCloseModal.length; i++) {
     overlays[i].classList.add("hidden");
   });
 }
+
+//CODE FOR LETTER BUTTON
+
+const letter = document.querySelector(".letterbutton");
+letter.addEventListener("click", function () {
+  const content = this.nextElementSibling;
+
+  if (content.style.display === "block") {
+    content.style.display = "none";
+  } else {
+    content.style.display = "block";
+  }
+});
+
+//change background color on scroll
+//from david deloso's 2020 fall mag
+const header = new Waypoint({
+  element: document.getElementById("content"),
+  handler: function (direction) {
+    gsap.to("body", { backgroundColor: "#dbebf0" });
+    gsap.to(".nbn-path", { fill: "#2d5355" });
+    gsap.to(".sectionheading", { color: "#2d5355" });
+    gsap.to(".sectioncontent", { border: "30px solid #2d5355" });
+  },
+});
+
+const pregameStart = new Waypoint({
+  element: document.getElementById("pregame-start"),
+  handler: function (direction) {
+    gsap.to("body", { backgroundColor: "#ebebeb" });
+    gsap.to(".nbn-path", { fill: "#e47d6d" });
+    gsap.to(".sectionheading", { color: "#e47d6d" });
+    gsap.to(".sectioncontent", { border: "30px solid #e47d6d" });
+  },
+});
+const pregameEnd = new Waypoint({
+  element: document.getElementById("pregame-end"),
+  handler: function (direction) {
+    gsap.to("body", { backgroundColor: "#ebebeb" });
+    gsap.to(".nbn-path", { fill: "#e47d6d" });
+    gsap.to(".sectionheading", { color: "#e47d6d" });
+    gsap.to(".sectioncontent", { border: "30px solid #e47d6d" });
+  },
+});
+const dancefloorStart = new Waypoint({
+  element: document.getElementById("dancefloor-start"),
+  handler: function (direction) {
+    gsap.to("body", { backgroundColor: "#ebebeb" });
+    gsap.to(".nbn-path", { fill: "#b46e75" });
+    gsap.to(".sectionheading", { color: "#b46e75" });
+    gsap.to(".sectioncontent", { border: "30px solid #b46e75" });
+  },
+});
+const dancefloorEnd = new Waypoint({
+  element: document.getElementById("dancefloor-end"),
+  handler: function (direction) {
+    gsap.to("body", { backgroundColor: "#ebebeb" });
+    gsap.to(".nbn-path", { fill: "#b46e75" });
+    gsap.to(".sectionheading", { color: "#b46e75" });
+    gsap.to(".sectioncontent", { border: "30px solid #b46e75" });
+  },
+});
+
+const featuresStart = new Waypoint({
+  element: document.getElementById("features-start"),
+  handler: function (direction) {
+    gsap.to("body", { backgroundColor: "black" });
+    gsap.to(".nbn-path", { fill: "white" });
+    gsap.to(".sectionheading", { color: "white" });
+    gsap.to(".sectioncontent", { border: "30px solid black" });
+  },
+});
+const featuresEnd = new Waypoint({
+  element: document.getElementById("features-end"),
+  handler: function (direction) {
+    gsap.to("body", { backgroundColor: "black" });
+    gsap.to(".nbn-path", { fill: "white" });
+    gsap.to(".sectionheading", { color: "white" });
+    gsap.to(".sectioncontent", { border: "30px solid black" });
+  },
+});
+
+const hangover = new Waypoint({
+  element: document.getElementById("hangover-start"),
+  handler: function (direction) {
+    gsap.to("body", { backgroundColor: "#dbebf0" });
+    gsap.to(".nbn-path", { fill: "#2d5355" });
+    gsap.to(".sectionheading", { color: "#2d5355" });
+    gsap.to(".sectioncontent", { border: "30px solid #2d5355" });
+  },
+});
+
+//LINKS
 let pregame1 = "/Pregame/Big10Surge/index.html";
 let pregame2 = "burgerking";
 let pregame3 = "marriagepact";
